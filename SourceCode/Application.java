90aefc0926f11f5c63117c26a8785349c5572d9f
Ankit Kumar <ankit.kumar.btech21@gmail.com>
2020-05-11 00:11:34 +0530

    Skeleton001: Dummy Main Method
    	A dummy main function is created for testing purpose.

---
+++ SourceCode/Application.java
@@ -0,0 +1,10 @@
+package SourceCode;
+
+public class Application{
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Payroll Application ");
+        System.out.println();
+        System.out.println("Application is under construction ");
+    }
+}
